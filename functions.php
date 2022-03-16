22ab9b4cc122671a5896ab517f6a7dff30a8d091
creed1120 <cedricareed@gmail.com>
2022-03-16 09:45:04 -0700

    portal setup

--- functions.php
+++ functions.php
@@ -1,4 +1,62 @@
 <?php
+/**
+ * MyFirstTheme's functions and definitions
+ *
+ * @package MyFirstTheme
+ * @since MyFirstTheme 1.0
+ */
+ 
+/**
+ * First, let's set the maximum content width based on the theme's design and stylesheet.
+ * This will limit the width of all uploaded images and embeds.
+ */
+if ( ! isset( $content_width ) )
+    $content_width = 800; /* pixels */
+ 
+if ( ! function_exists( 'vertexgitsetup_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ */
+function vertexgitsetup_setup() {
+ 
+    /**
+     * Make theme available for translation.
+     * Translations can be placed in the /languages/ directory.
+     */
+    load_theme_textdomain( 'vertexgitsetup', get_template_directory() . '/languages' );
+ 
+    /**
+     * Add default posts and comments RSS feed links to &lt;head>.
+     */
+    add_theme_support( 'automatic-feed-links' );
+ 
+    /**
+     * Enable support for post thumbnails and featured images.
+     */
+    add_theme_support( 'post-thumbnails' );
+ 
+    /**
+     * Add support for two custom navigation menus.
+     */
+    register_nav_menus( array(
+        'primary'   => __( 'Primary Menu', 'vertexgitsetup' ),
+        'top-footer' => __('Top Footer Menu', 'vertexgitsetup' )
+    ) );
+ 
+    /**
+     * Enable support for the following post formats:
+     * aside, gallery, quote, image, and video
+     */
+    add_theme_support( 'post-formats', array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
+}
+
+endif; // vertexgitsetup_setup
+add_action( 'after_setup_theme', 'vertexgitsetup_setup' );
+
 
 // Enqueue Styles and Scripts
 function tailwindwp_add_styles() {
---
+++ header.php
@@ -0,0 +1,40 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ECKM Portal Project</title>
+
+        <?php wp_head(); ?>
+    </head>
+
+<body>
+
+    <!-- Top Main Header Section -->
+    <section class="w-full px-8 text-gray-700 bg-white">
+        <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
+            <div class="relative flex flex-col md:flex-row">
+                <a href="#_" class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
+                    <span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">Legacy Traditional<span class="text-indigo-600">.</span></span>
+                </a>
+                <nav class="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
+                    
+                <?php
+                    wp_nav_menu( array(
+                        'theme_location' => "primary"
+                    ) );
+                ?>
+
+                </nav>
+            </div>
+
+            <!-- <div class="inline-flex items-center ml-5 space-x-6 lg:justify-end">
+                <a href="#" class="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900">
+                    Sign in
+                </a>
+                <a href="#" class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
+                    Sign up
+                </a>
+            </div> -->
+        </div>
+    </section>
