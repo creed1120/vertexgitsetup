05f1ad4b3732f952db855065dd659c2bc879adeb
Cedric <cedricareed@gmail.com>
2022-04-23 12:25:03 -0700

    renamed files

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
         <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
             <div class="relative flex flex-col md:flex-row">
                 <a href="#_" class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
-                    <span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">Legacy Traditional<span class="text-indigo-600">.</span></span>
+                    <span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">Wordpress Learning<span class="text-indigo-600">.</span></span>
                 </a>
                 <nav class="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
                     
